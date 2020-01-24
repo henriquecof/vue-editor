7b7c2c9fa08ae8f6eb3abd41f5ac344377a078a9
Henrique Almeida <almeida.cof@gmail.com>
2020-01-24 15:42:05 -0300

    configuração emoji editor

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -1,68 +1,70 @@
 <template>
-    <div class="editor">
-        <quill-editor
-            v-model="content"
-            :options="editorOption">
-        </quill-editor>
-    </div>
-        
+  <div class="editor">
+    <quill-editor v-model="content" :options="editorOption"></quill-editor>
+  </div>
 </template>
 
 <script>
 export default {
-    name: "Editor",
-    methods: {},
-    data() {
-        return {
-            content: "",
-            editorOption: {
-                modules: {
-                    toolbar: {
-                        container: [
-                            ["bold", "italic", "underline", "strike"],
-                            ["blockquote", "code-block"],
-                            [{header: 1}, {header: 2}],
-                            [{list: "ordered"}, {list: "bullet"}],
-                            [{script: "sub"}, {script: "super"}],
-                            [{indent: "-1"}, {indent: "+1"}],
-                            [{direction:"rtl"}],
-                            [{size:["smal", false, "large", "huge"]}],
-                            [{header: [1,2,3,4,5,6, false]}],
-                            [{font:[]}],
-                            [{color:[]}, {background: []}],
-                            [{align:[]}],
-                            ["clean"],
-                            ["link", "image", "video"]
-
-
-                        ],
-                    },
-                }
-
-            }
+  name: "Editor",
+  methods: {},
+  data() {
+    return {
+      content: "",
+      editorOption: {
+        modules: {
+          toolbar: {
+            container: [
+              ["bold", "italic", "underline", "strike"],
+              ["blockquote", "code-block"],
+              [{ header: 1 }, { header: 2 }],
+              [{ list: "ordered" }, { list: "bullet" }],
+              [{ script: "sub" }, { script: "super" }],
+              [{ indent: "-1" }, { indent: "+1" }],
+              [{ direction: "rtl" }],
+              [{ size: ["smal", false, "large", "huge"] }],
+              [{ header: [1, 2, 3, 4, 5, 6, false] }],
+              [{ font: [] }],
+              [{ color: [] }, { background: [] }],
+              [{ align: [] }],
+              ["clean"],
+              ["link", "image", "video"]
+            ],
+            handlers: { emoji: () => {} }
+          },
+          //plugin drag and drop
+          imageDrop: true,
+          //plugin resize
+          imageResize: {
+            modules: ["Resize", "DisplaySize", "Toolbar"]
+          },
+          "emoji-toolbar": true,
+          "emoji-shotname": true
         }
-    }
+      }
     };
+  }
+};
 </script>
 
 
 <style>
-.quill-editor, .quill-code {
-    width: 100%;
-    height: 90vh;
+.quill-editor,
+.quill-code {
+  width: 100%;
+  height: 90vh;
 }
 .quill-code {
-    height: auto;
-    border: none;
+  height: auto;
+  border: none;
 }
 
 .quill-code > .title {
-    border: 1px solid #ccc;
-    border-left: none;
-    height: 3em;
-    line-height: 3em;
-    text-indent: 1rem;
-    font-weight: bold;
+  border: 1px solid #ccc;
+  border-left: none;
+  height: 3em;
+  line-height: 3em;
+  text-indent: 1rem;
+  font-weight: bold;
 }
-
 </style>
